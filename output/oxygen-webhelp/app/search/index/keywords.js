7dbe4bf59cf0086e18c03aaebb3fb6eee0233903
JakubBuk <92690955+JakubBuk@users.noreply.github.com>
2022-03-13 11:45:14 +0100

    naming

---
+++ output/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,12 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"My",p:["p1","p2","p3"]},{w:"first",p:["p1","p2","p3"]},{w:"map",p:["p1"]},{w:"topic",p:["p2"]},{w:"reference",p:["p3"]},{w:"Doing",p:["p4"]},{w:"something",p:["p4"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4, 5];
+ph["p2"]=[3, 4, 6];
+ph["p3"]=[3, 4, 7];
+ph["p4"]=[8, 9];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
