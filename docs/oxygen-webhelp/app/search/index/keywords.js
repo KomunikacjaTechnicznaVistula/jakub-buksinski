372ad29c93034c05057e5ff2234c25a19d267999
JakubBuk <92690955+JakubBuk@users.noreply.github.com>
2022-03-22 23:07:49 +0100

    G project

--- docs/oxygen-webhelp/app/search/index/keywords.js
+++ docs/oxygen-webhelp/app/search/index/keywords.js
@@ -1,10 +1,22 @@
-define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"My",p:["p1","p2","p3"]},{w:"first",p:["p1","p2","p3"]},{w:"map",p:["p1"]},{w:"topic",p:["p2"]},{w:"reference",p:["p3"]},{w:"Doing",p:["p4"]},{w:"something",p:["p4"]}];
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Popular",p:["p1"]},{w:"guitar",p:["p1"]},{w:"models",p:["p1"]},{w:"Fender",p:["p2","p3","p8","p9","p10","p11","p13"]},{w:"Stratocaster",p:["p2"]},{w:"Telecaster",p:["p3"]},{w:"Squier",p:["p4","p5"]},{w:"Jazzmaster",p:["p4","p10"]},{w:"Jaguar",p:["p5","p8"]},{w:"Gibson",p:["p6","p14"]},{w:"Les",p:["p6"]},{w:"Paul",p:["p6"]},{w:"Gretsch",p:["p7"]},{w:"6120",p:["p7"]},{w:"fretless",p:["p9"]},{w:"Jazz",p:["p9"]},{w:"Bass",p:["p9","p13"]},{w:"Mustang",p:["p11"]},{w:"My",p:["p12","p15"]},{w:"first",p:["p12","p15"]},{w:"topic",p:["p12"]},{w:"Precision",p:["p13"]},{w:"SG",p:["p14"]},{w:"reference",p:["p15"]},{w:"Doing",p:["p16"]},{w:"something",p:["p16"]}];
 var ph={};
 ph["p0"]=[0, 1, 2];
 ph["p1"]=[3, 4, 5];
-ph["p2"]=[3, 4, 6];
-ph["p3"]=[3, 4, 7];
-ph["p4"]=[8, 9];
+ph["p2"]=[6, 7];
+ph["p3"]=[6, 8];
+ph["p4"]=[9, 10];
+ph["p5"]=[9, 11];
+ph["p6"]=[12, 13, 14];
+ph["p7"]=[15, 16];
+ph["p8"]=[6, 11];
+ph["p9"]=[6, 17, 18, 19];
+ph["p10"]=[6, 10];
+ph["p12"]=[21, 22, 23];
+ph["p11"]=[6, 20];
+ph["p14"]=[12, 25];
+ph["p13"]=[6, 24, 19];
+ph["p16"]=[27, 28];
+ph["p15"]=[21, 22, 26];
      return {
          keywords: keywords,
          ph: ph
--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++ docs/oxygen-webhelp/app/search/index/link-to-parent.js
@@ -1,4 +1,4 @@
 /*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
 define(function () {
-return {2:-1,4:-1,3:-1};
+return {17:-1,8:-1,10:-1,9:-1,16:-1,13:-1,18:-1,15:-1,11:-1,12:-1};
 });
