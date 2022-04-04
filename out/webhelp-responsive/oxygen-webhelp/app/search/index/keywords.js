12740401b9044629300e88ceceaaedce547e8823
JakubBuk <92690955+JakubBuk@users.noreply.github.com>
2022-04-04 23:00:14 +0200

    f

---
+++ out/webhelp-responsive/oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,18 @@
+define(function() {var keywords=[{w:"Context",p:["p0"]},{w:"Sensitive",p:["p0"]},{w:"Help",p:["p0"]},{w:"Gretsch",p:["p1"]},{w:"6120",p:["p1"]},{w:"Fender",p:["p2","p3","p4","p6","p7","p9","p10"]},{w:"Jaguar",p:["p2"]},{w:"fretless",p:["p3"]},{w:"Jazz",p:["p3"]},{w:"Bass",p:["p3","p7"]},{w:"Jazzmaster",p:["p4"]},{w:"Gibson",p:["p5","p8"]},{w:"Les",p:["p5"]},{w:"Paul",p:["p5"]},{w:"Mustang",p:["p6"]},{w:"Precision",p:["p7"]},{w:"SG",p:["p8"]},{w:"Stratocaster",p:["p9"]},{w:"Telecaster",p:["p10"]}];
+var ph={};
+ph["p0"]=[0, 1, 2];
+ph["p1"]=[3, 4];
+ph["p2"]=[5, 6];
+ph["p3"]=[5, 7, 8, 9];
+ph["p4"]=[5, 10];
+ph["p5"]=[11, 12, 13];
+ph["p6"]=[5, 14];
+ph["p7"]=[5, 15, 9];
+ph["p8"]=[11, 16];
+ph["p9"]=[5, 17];
+ph["p10"]=[5, 18];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
