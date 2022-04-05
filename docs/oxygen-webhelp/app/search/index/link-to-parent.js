c5409cfdd7de479dc761bf1d44076afe1153124d
JakubBuk <92690955+JakubBuk@users.noreply.github.com>
2022-04-05 23:03:54 +0200

    s

--- docs/oxygen-webhelp/app/search/index/link-to-parent.js
+++
@@ -1,4 +0,0 @@
-/*Maps current topic to its parent: "topicIndex:parentIndex". -1 represents the map.*/
-define(function () {
-return {17:-1,8:-1,10:-1,9:-1,16:-1,13:-1,18:-1,15:-1,11:-1,12:-1};
-});
